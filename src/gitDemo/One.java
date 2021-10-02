e437a86f285890492ad86a34a4aa5286aa212209
aandagandg <aandagandg@mail.ru>
2021-10-02 15:11:11 -0400

    .

--- src/gitDemo/One.java
+++ src/gitDemo/One.java
@@ -1,9 +1,15 @@
 package gitDemo;
 
 public class One {
-public static void main(String[] args) {
-	System.out.println("Any. ogh");
-	// comment
-	System.out.println("new code");
-}
+	public static void main(String[] args) {
+		System.out.println("Any. ogh");
+		// comment
+		System.out.println("new code");
+
+		addNum(2, 5);
+	}
+
+	public static void addNum(int a, int b) {
+		System.out.println(a + b);
+	}
 }
